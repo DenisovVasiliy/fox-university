630a1d19bbeb9c2a047ee7ab862024a33565ed73
VasyaDen <vasyaden@mail.ru>
2020-03-28 13:11:53 +0300

    CourseDAO was updated for using Spring. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/dao/CourseDAO.java
+++ src/main/java/com/foxminded/foxuniversity/dao/CourseDAO.java
@@ -1,18 +1,20 @@
 package com.foxminded.foxuniversity.dao;
 
-import com.foxminded.foxuniversity.dao.infra.DataSource;
 import com.foxminded.foxuniversity.domain.Course;
-import com.foxminded.foxuniversity.domain.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class CourseDAO {
-    DataSource dataSource = DataSource.getInstance();
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource.getDataSource());
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public List<Course> getAll() {
         return jdbcTemplate.query("select * from courses;", new RowMapper<Course>() {
