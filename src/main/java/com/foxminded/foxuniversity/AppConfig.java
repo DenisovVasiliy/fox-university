eb7b98008196e23d111e1b9274175df856931987
VasyaDen <vasyaden@mail.ru>
2020-03-28 13:09:20 +0300

    AppConfig was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/main/java/com/foxminded/foxuniversity/AppConfig.java
@@ -0,0 +1,41 @@
+package com.foxminded.foxuniversity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan({"com.foxminded.foxuniversity"})
+@PropertySource("classpath:database.properties")
+public class AppConfig {
+
+    @Autowired
+    private Environment environment;
+
+    private final String DRIVER = "driver";
+    private final String URL = "url";
+    private final String USER = "user";
+    private final String PASSWORD = "password";
+
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty(DRIVER));
+        dataSource.setUrl(environment.getProperty(URL));
+        dataSource.setUsername(environment.getProperty(USER));
+        dataSource.setPassword(environment.getProperty(PASSWORD));
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
