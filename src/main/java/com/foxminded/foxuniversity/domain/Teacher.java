2cf5e0dc10eebc89723dbe6b78904669e6dca27b
VasyaDen <vasyaden@mail.ru>
2020-03-30 16:58:24 +0300

    Teacher.setId() method was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/domain/Teacher.java
+++ src/main/java/com/foxminded/foxuniversity/domain/Teacher.java
@@ -17,6 +17,10 @@ public class Teacher {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
