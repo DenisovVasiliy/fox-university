c74e47ee94e6a01449fddb9a28f2646af7f864b6
VasyaDen <vasyaden@mail.ru>
2020-04-04 15:00:31 +0300

    Init DB script Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/main/resources/createTables.sql
@@ -0,0 +1,63 @@
+CREATE TABLE IF NOT EXISTS groups
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL UNIQUE CHECK (name != '')
+);
+
+CREATE TABLE IF NOT EXISTS students
+(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL CHECK (first_name != ''),
+  last_name VARCHAR(30) NOT NULL CHECK (last_name != '')
+);
+
+CREATE TABLE IF NOT EXISTS courses
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL CHECK (name != ''),
+  description VARCHAR(200) NOT NULL CHECK (description != '')
+);
+
+CREATE TABLE IF NOT EXISTS groups_courses
+(
+  group_id INTEGER,
+  course_id INTEGER,
+  UNIQUE (group_id , course_id),
+  FOREIGN KEY (group_id) REFERENCES groups(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (course_id) REFERENCES courses(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS students_groups
+(
+  student_id INTEGER UNIQUE,
+  group_id INTEGER,
+  FOREIGN KEY (student_id) REFERENCES students(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (group_id) REFERENCES groups(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS teachers
+(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL CHECK (first_name != ''),
+  last_name VARCHAR(30) NOT NULL CHECK (last_name != ''),
+  course_id INTEGER,
+  FOREIGN KEY (course_id) REFERENCES courses(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS lessons
+(
+  id SERIAL PRIMARY KEY,
+  teacher_id INTEGER,
+  classroom INTEGER,
+  day VARCHAR(30) NOT NULL CHECK (day != ''),
+  type VARCHAR(30) NOT NULL CHECK (day != ''),
+  FOREIGN KEY (teacher_id) REFERENCES teachers(id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS courses_lessons
+(
+  course_id INTEGER,
+  lesson_id INTEGER UNIQUE,
+  FOREIGN KEY (course_id) REFERENCES courses(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (lesson_id) REFERENCES lessons(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
