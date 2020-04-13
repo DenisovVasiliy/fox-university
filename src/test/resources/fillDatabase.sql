1cbad08f2a93cb6fd2b4a8b73d500c7ebe5af543
VasyaDen <vasyaden@mail.ru>
2020-04-13 19:10:23 +0300

    Script for testDB was updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/test/resources/fillDatabase.sql
+++ src/test/resources/fillDatabase.sql
@@ -6,10 +6,10 @@ insert into students (first_name, last_name) values ('S-01', 'Student'), ('S-02'
 
 insert into teachers (first_name, last_name, course_id) values ('T-01', 'Teacher', 1), ('T-02', 'Teacher', 2), ('T-03', 'Teacher', 2);
 
-insert into lessons (course_id, teacher_id, classroom, day, time, type) values (1, 1, 10, 'MONDAY', '09:30:00', 'LECTURE'), (2, 1, 10, 'FRIDAY', '09:30:00', 'LECTURE'), (2, 2, 20, 'FRIDAY', '09:30:00', 'LECTURE');
+insert into lessons (course_id, teacher_id, classroom, day, time, type) values (1, 1, 10, 'MONDAY', '09:30:00', 'LECTURE'), (2, 1, 10, 'TUESDAY', '09:30:00', 'LECTURE'), (2, 2, 20, 'FRIDAY', '09:30:00', 'LECTURE');
 
 insert into students_groups (student_id, group_id) values (1, 1), (2, 1), (3, 2);
 
-insert into groups_courses (group_id, course_id) values (1, 1), (1, 2), (2, 2), (3, 3);
+insert into groups_courses (group_id, course_id) values (1, 1), (1, 2), (2, 2);
 
-insert into lessons_groups (lesson_id, group_id) values (1, 1), (1, 2), (2, 3);
+insert into lessons_groups (lesson_id, group_id) values (1, 1), (2, 1), (2, 2), (3, 2);
