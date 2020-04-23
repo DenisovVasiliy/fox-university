7458c98f215e3c5289716e4df13a923473197ab2
VasyaDen <vasyaden@mail.ru>
2020-04-23 17:27:28 +0300

    GroupDao.getByCourse Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/dao/GroupDao.java
+++ src/main/java/com/foxminded/foxuniversity/dao/GroupDao.java
@@ -16,4 +16,6 @@ public interface GroupDao extends GenericDao<Group> {
     boolean deleteFromCourse(Group group, List<Course> courses);
 
     List<Group> getByLesson(Lesson lesson);
+
+    List<Group> getByCourse(Course course);
 }
