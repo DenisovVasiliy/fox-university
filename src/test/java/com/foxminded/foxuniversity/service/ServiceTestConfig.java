f429cf7018cbd8e20299d4cb7fc601d0f3c4c6ca
VasyaDen <vasyaden@mail.ru>
2020-05-09 19:05:43 +0300

    ServiceTestConfig was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/test/java/com/foxminded/foxuniversity/service/ServiceTestConfig.java
@@ -0,0 +1,10 @@
+package com.foxminded.foxuniversity.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.foxminded.foxuniversity.service", "com.foxminded.foxuniversity.dao"})
+public class ServiceTestConfig {
+
+}
