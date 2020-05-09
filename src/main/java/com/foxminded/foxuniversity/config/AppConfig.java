1ab655e3a4781ca9ea8081efa509a54e32fb3e4b
VasyaDen <vasyaden@mail.ru>
2020-05-09 20:01:51 +0300

    Configs were merged to AppConfig. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/config/AppConfig.java
+++ src/main/java/com/foxminded/foxuniversity/config/AppConfig.java
@@ -1,24 +1,30 @@
 package com.foxminded.foxuniversity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan(basePackages = "com.foxminded.foxuniversity",
-        excludeFilters = @ComponentScan.Filter(type= FilterType.REGEX,
-                pattern="com\\.foxminded\\.foxuniversity\\.controllers\\..*"))
+@ComponentScan("com.foxminded.foxuniversity")
+@EnableWebMvc
 @PropertySource("classpath:database.properties")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Value("${url}")
     private String url;
@@ -28,6 +34,13 @@ public class AppConfig {
     private String user;
     @Value("${password}")
     private String password;
+
+    private final ApplicationContext context;
+
+    @Autowired
+    public AppConfig(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Bean
     DataSource dataSource() {
@@ -48,5 +61,29 @@ public class AppConfig {
     @Scope("prototype")
     public SimpleJdbcInsert jdbcInsert() {
         return new SimpleJdbcInsert(dataSource());
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(context);
+        templateResolver.setPrefix("WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
     }
 }
