d17a74b087175eb32e371eab8287000c6268e6f1
VasyaDen <vasyaden@mail.ru>
2020-05-11 22:32:37 +0300

    CourseController was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/main/java/com/foxminded/foxuniversity/controllers/CourseController.java
@@ -0,0 +1,43 @@
+package com.foxminded.foxuniversity.controllers;
+
+import com.foxminded.foxuniversity.domain.*;
+import com.foxminded.foxuniversity.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+@Controller
+@RequestMapping("/courses")
+public class CourseController {
+    private CourseService courseService;
+    private TeacherService teacherService;
+
+    @Autowired
+    public CourseController(CourseService courseService, TeacherService teacherService) {
+        this.courseService = courseService;
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping
+    public String showAllCourses(Model model) {
+        List<Course> courses = courseService.getAll();
+        model.addAttribute("courses", courses);
+        return "courses/courses";
+    }
+
+    @GetMapping("/{id}")
+    public String showCourseById(Model model, @PathVariable String id) {
+        Course course = courseService.getById(parseInt(id));
+        List<Teacher> teachers = teacherService.getByCourse(course);
+        model.addAttribute("course", course);
+        model.addAttribute("teachers", teachers);
+        return "courses/oneCourse";
+    }
+}
