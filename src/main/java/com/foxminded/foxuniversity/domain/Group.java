042b2ff3c0810811235b53718eefc06140e849dc
VasyaDen <vasyaden@mail.ru>
2020-05-13 18:22:21 +0300

    Cleaning (Group) Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/domain/Group.java
+++ src/main/java/com/foxminded/foxuniversity/domain/Group.java
@@ -8,15 +8,15 @@ public class Group {
     private String name;
     private List<Student> students = new ArrayList<>();
 
-    public Group (int id) {
+    public Group(int id) {
         this.id = id;
     }
 
-    public Group (String name) {
+    public Group(String name) {
         this.name = name;
     }
 
-    public Group (int id, String name) {
+    public Group(int id, String name) {
         this.id = id;
         this.name = name;
     }
