fecea0a76c30e8934aec11395ac48b8911f4332c
VasyaDen <vasyaden@mail.ru>
2020-05-13 13:22:01 +0300

    Exceptions was added to CourseDaoPostgres. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/dao/implementation/CourseDaoPostgres.java
+++ src/main/java/com/foxminded/foxuniversity/dao/implementation/CourseDaoPostgres.java
@@ -1,12 +1,16 @@
 package com.foxminded.foxuniversity.dao.implementation;
 
 import com.foxminded.foxuniversity.dao.CourseDao;
+import com.foxminded.foxuniversity.dao.exceptions.EntityNotFoundException;
+import com.foxminded.foxuniversity.dao.exceptions.QueryNotExecuteException;
 import com.foxminded.foxuniversity.dao.mappers.CourseMapper;
 import com.foxminded.foxuniversity.domain.Course;
 import com.foxminded.foxuniversity.domain.Group;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -14,6 +18,9 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.foxminded.foxuniversity.dao.exceptions.ExceptionsMessageConstants.*;
+import static java.lang.String.format;
 
 @Repository
 @PropertySource("classpath:queries.properties")
@@ -34,6 +41,8 @@ public class CourseDaoPostgres implements CourseDao {
     @Value("${course.delete}")
     private String delete;
 
+    private String entity = "Course";
+
     @Autowired
     public CourseDaoPostgres(JdbcTemplate jdbcTemplate, SimpleJdbcInsert jdbcInsert, CourseMapper courseMapper) {
         this.jdbcTemplate = jdbcTemplate;
@@ -43,33 +52,66 @@ public class CourseDaoPostgres implements CourseDao {
 
     @Override
     public List<Course> getAll() {
-        return jdbcTemplate.query(getAll, courseMapper);
+        try {
+            return jdbcTemplate.query(getAll, courseMapper);
+        } catch (DataAccessException e) {
+            String msg = format(UNABLE_GET_ALL, entity);
+            throw new QueryNotExecuteException(msg, e);
+        }
     }
 
     @Override
     public Course getById(int id) {
-        return jdbcTemplate.queryForObject(getById, new Object[]{id}, courseMapper);
+        try {
+            return jdbcTemplate.queryForObject(getById, new Object[]{id}, courseMapper);
+        } catch (EmptyResultDataAccessException e) {
+            String msg = format(ENTITY_NOT_FOUND, entity, id);
+            throw new EntityNotFoundException(msg);
+        } catch (DataAccessException e) {
+            String msg = format(UNABLE_GET_BY_ID, entity, id);
+            throw new QueryNotExecuteException(msg, e);
+        }
     }
 
     @Override
     public List<Course> getByGroup(Group group) {
-        return jdbcTemplate.query(getByGroup, new Object[]{group.getId()}, courseMapper);
+        try {
+            return jdbcTemplate.query(getByGroup, new Object[]{group.getId()}, courseMapper);
+        } catch (DataAccessException e) {
+            String msg = format(UNABLE_GET_BY_ENTITY, entity, group.toString());
+            throw new QueryNotExecuteException(msg, e);
+        }
     }
 
     @Override
     public boolean delete(Course course) {
-        return jdbcTemplate.update(delete, course.getId()) > 0;
+        try {
+            return jdbcTemplate.update(delete, course.getId()) > 0;
+        } catch (DataAccessException e) {
+            String msg = format(UNABLE_DELETE, course.toString());
+            throw new QueryNotExecuteException(msg, e);
+        }
     }
 
     @Override
     public void save(Course course) {
-        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(course);
-        Number id = jdbcInsert.executeAndReturnKey(parameterSource);
-        course.setId(id.intValue());
+        try {
+            SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(course);
+            Number id = jdbcInsert.executeAndReturnKey(parameterSource);
+            course.setId(id.intValue());
+        } catch (DataAccessException e) {
+            String msg = format(UNABLE_SAVE, course.toString());
+            throw new QueryNotExecuteException(msg, e);
+        }
     }
 
     @Override
     public boolean update(Course course) {
-        return jdbcTemplate.update(update, course.getName(), course.getDescription(), course.getId()) > 0;
+        try {
+            return jdbcTemplate.update(update, course.getName(), course.getDescription(), course.getId()) > 0;
+        } catch (DataAccessException e) {
+            String msg = format(UNABLE_UPDATE, course.toString());
+            throw new QueryNotExecuteException(msg, e);
+        }
     }
 }
