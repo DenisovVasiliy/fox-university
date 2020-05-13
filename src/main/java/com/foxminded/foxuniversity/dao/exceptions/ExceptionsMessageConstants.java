77e9ea8ba28158311f56f8de9a396c830781f10f
VasyaDen <vasyaden@mail.ru>
2020-05-13 14:37:46 +0300

    ExceptionsMessageConstants was updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/dao/exceptions/ExceptionsMessageConstants.java
+++ src/main/java/com/foxminded/foxuniversity/dao/exceptions/ExceptionsMessageConstants.java
@@ -3,10 +3,10 @@ package com.foxminded.foxuniversity.dao.exceptions;
 public class ExceptionsMessageConstants {
     private ExceptionsMessageConstants() { }
 
-    public static final String UNABLE_GET_ALL = "Unable to get all '%s's";
-    public static final String ENTITY_NOT_FOUND = "'%s' with id '%s' not found";
-    public static final String UNABLE_GET_BY_ID = "Unable to get '%s' with ID '%s'";
-    public static final String UNABLE_GET_BY_ENTITY = "Unable to get '%s' by '%s'";
+    public static final String UNABLE_GET_ALL = "Unable to get all %ss";
+    public static final String ENTITY_NOT_FOUND = "%s with ID '%s' not found";
+    public static final String UNABLE_GET_BY_ID = "Unable to get %s with ID '%s'";
+    public static final String UNABLE_GET_BY_ENTITY = "Unable to get %s by '%s'";
     public static final String UNABLE_DELETE = "Unable to delete '%s'";
     public static final String UNABLE_SAVE = "Unable to save '%s'";
     public static final String UNABLE_UPDATE  = "Unable to update '%s'";
