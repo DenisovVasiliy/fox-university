d96c682789e5beba950e9d04d2a49ce6a894b07b
VasyaDen <vasyaden@mail.ru>
2020-05-14 22:05:47 +0300

    Logging by Lombok (CourseDaoPostgres). Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/dao/implementation/CourseDaoPostgres.java
+++ src/main/java/com/foxminded/foxuniversity/dao/implementation/CourseDaoPostgres.java
@@ -6,8 +6,7 @@ import com.foxminded.foxuniversity.dao.exceptions.QueryNotExecuteException;
 import com.foxminded.foxuniversity.dao.mappers.CourseMapper;
 import com.foxminded.foxuniversity.domain.Course;
 import com.foxminded.foxuniversity.domain.Group;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -26,6 +25,7 @@ import static java.lang.String.format;
 
 @Repository
 @PropertySource("classpath:queries.properties")
+@Slf4j
 public class CourseDaoPostgres implements CourseDao {
 
     private JdbcTemplate jdbcTemplate;
@@ -44,7 +44,6 @@ public class CourseDaoPostgres implements CourseDao {
     private String delete;
 
     private String entity = "Course";
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public CourseDaoPostgres(JdbcTemplate jdbcTemplate, SimpleJdbcInsert jdbcInsert, CourseMapper courseMapper) {
@@ -55,95 +54,95 @@ public class CourseDaoPostgres implements CourseDao {
 
     @Override
     public List<Course> getAll() {
-        LOGGER.debug("getAll()");
+        log.debug("getAll()");
         List<Course> courses;
         try {
             courses = jdbcTemplate.query(getAll, courseMapper);
         } catch (DataAccessException e) {
             String msg = format(UNABLE_GET_ALL, entity);
-            LOGGER.error(msg);
+            log.error(msg);
             throw new QueryNotExecuteException(msg, e);
         }
-        LOGGER.trace("Found {} courses", courses.size());
+        log.trace("Found {} courses", courses.size());
         return courses;
     }
 
     @Override
     public Course getById(int id) {
-        LOGGER.debug("getById({})", id);
+        log.debug("getById({})", id);
         Course course;
         try {
             course = jdbcTemplate.queryForObject(getById, new Object[]{id}, courseMapper);
         } catch (EmptyResultDataAccessException e) {
             String msg = format(ENTITY_NOT_FOUND, entity, id);
-            LOGGER.warn(msg);
+            log.warn(msg);
             throw new EntityNotFoundException(msg);
         } catch (DataAccessException e) {
             String msg = format(UNABLE_GET_BY_ID, entity, id);
-            LOGGER.error(msg);
+            log.error(msg);
             throw new QueryNotExecuteException(msg, e);
         }
-        LOGGER.trace("Found {}", course);
+        log.trace("Found {}", course);
         return course;
     }
 
     @Override
     public List<Course> getByGroup(Group group) {
-        LOGGER.debug("getByGroup({})", group);
+        log.debug("getByGroup({})", group);
         List<Course> courses;
         try {
             courses = jdbcTemplate.query(getByGroup, new Object[]{group.getId()}, courseMapper);
         } catch (DataAccessException e) {
             String msg = format(UNABLE_GET_BY_ENTITY, entity, group.toString());
-            LOGGER.error(msg);
+            log.error(msg);
             throw new QueryNotExecuteException(msg, e);
         }
-        LOGGER.trace("Found {} courses", courses.size());
+        log.trace("Found {} courses", courses.size());
         return courses;
     }
 
     @Override
     public boolean delete(Course course) {
-        LOGGER.debug("delete({})", course);
+        log.debug("delete({})", course);
         int counter;
         try {
             counter = jdbcTemplate.update(delete, course.getId());
         } catch (DataAccessException e) {
             String msg = format(UNABLE_DELETE, course.toString());
-            LOGGER.error(msg);
+            log.error(msg);
             throw new QueryNotExecuteException(msg, e);
         }
-        LOGGER.trace("Deleted '{}' {}", counter, course);
+        log.trace("Deleted '{}' {}", counter, course);
         return counter > 0;
     }
 
     @Override
     public void save(Course course) {
-        LOGGER.debug("save({})", course);
+        log.debug("save({})", course);
         try {
             SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(course);
             Number id = jdbcInsert.executeAndReturnKey(parameterSource);
             course.setId(id.intValue());
         } catch (DataAccessException e) {
             String msg = format(UNABLE_SAVE, course.toString());
-            LOGGER.error(msg);
+            log.error(msg);
             throw new QueryNotExecuteException(msg, e);
         }
-        LOGGER.trace("{} saved.", course);
+        log.trace("{} saved.", course);
     }
 
     @Override
     public boolean update(Course course) {
-        LOGGER.debug("update({})", course);
+        log.debug("update({})", course);
         int counter;
         try {
             counter = jdbcTemplate.update(update, course.getName(), course.getDescription(), course.getId());
         } catch (DataAccessException e) {
             String msg = format(UNABLE_UPDATE, course.toString());
-            LOGGER.error(msg);
+            log.error(msg);
             throw new QueryNotExecuteException(msg, e);
         }
-        LOGGER.trace("Updated '{}' {}", counter, course);
+        log.trace("Updated '{}' {}", counter, course);
         return counter > 0;
     }
 }
