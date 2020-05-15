f84269f2db5d0aa43f6a7ae1ca68375813d46744
VasyaDen <vasyaden@mail.ru>
2020-05-15 18:01:21 +0300

    ExceptionsMessageConstants was updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/dao/exceptions/ExceptionsMessageConstants.java
+++ src/main/java/com/foxminded/foxuniversity/dao/exceptions/ExceptionsMessageConstants.java
@@ -8,9 +8,14 @@ public class ExceptionsMessageConstants {
     public static final String UNABLE_GET_BY_ID = "Unable to get %s with ID '%s'";
     public static final String UNABLE_GET_BY_ENTITY = "Unable to get %s by '%s'";
     public static final String UNABLE_DELETE = "Unable to delete '%s'";
-    public static final String DELETION_RESTRICTED = "Deletion is restricted: %s has relations to the %s";
+    public static final String DELETION_RESTRICTED = "Deletion was restricted: %s has relations to the %s";
+    public static final String QUERY_RESTRICTED_DUPLICATE_KEY = "Query was restricted: " +
+            "Unique index or primary key violation";
+    public static final String QUERY_RESTRICTED_NO_SUCH_ID = "Query was restricted: " +
+            "There is no such %s or %s";
     public static final String UNABLE_SAVE = "Unable to save '%s'";
     public static final String UNABLE_UPDATE  = "Unable to update '%s'";
     public static final String UNABLE_ASSIGN = "Unable assign %s to %s";
+    public static final String UNABLE_UPDATE_ASSIGNMENT = "Unable update assignment of %s to %s";
     public static final String UNABLE_DELETE_FROM = "Unable delete %s from %s";
 }
