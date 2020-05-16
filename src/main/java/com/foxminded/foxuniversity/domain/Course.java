02735372a5f3fdc87155665b4c77f580100377af
VasyaDen <vasyaden@mail.ru>
2020-05-16 14:07:08 +0300

    Course.class was updated (with Lombok). Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/domain/Course.java
+++ src/main/java/com/foxminded/foxuniversity/domain/Course.java
@@ -1,20 +1,25 @@
 package com.foxminded.foxuniversity.domain;
 
+import lombok.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Course {
+    @NonNull
+    @EqualsAndHashCode.Include
     private int id;
+    @NonNull
     private String name;
+    @NonNull
     private String description;
+    @ToString.Exclude
     private List<Lesson> lessons = new ArrayList<>();
+    @ToString.Exclude
     private List<Group> groups = new ArrayList<>();
-
-    public Course(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
 
     public Course(String name, String description) {
         this.name = name;
@@ -23,77 +28,5 @@ public class Course {
 
     public Course(int id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<Lesson> getLessons() {
-        return lessons;
-    }
-
-    public void setLessons(List<Lesson> lessons) {
-        this.lessons = lessons;
-    }
-
-    public List<Group> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(List<Group> groups) {
-        this.groups = groups;
-    }
-
-    public void addLessons(List<Lesson> lessons) {
-        for (Lesson lesson : lessons) {
-            if (!this.lessons.contains(lesson)) {
-                this.lessons.add(lesson);
-            }
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Course)) return false;
-
-        Course course = (Course) o;
-
-        return id == course.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Course{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 }
