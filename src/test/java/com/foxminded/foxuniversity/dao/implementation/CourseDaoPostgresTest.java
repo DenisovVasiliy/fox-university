d873f2268957bd24bd2502d19ef09467f8a4297a
VasyaDen <vasyaden@mail.ru>
2020-05-16 14:42:08 +0300

    Tests for thrown exceptions were added to CourseDaoPostgresTest. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/test/java/com/foxminded/foxuniversity/dao/implementation/CourseDaoPostgresTest.java
+++ src/test/java/com/foxminded/foxuniversity/dao/implementation/CourseDaoPostgresTest.java
@@ -2,6 +2,9 @@ package com.foxminded.foxuniversity.dao.implementation;
 
 import com.foxminded.foxuniversity.dao.CourseDao;
 import com.foxminded.foxuniversity.dao.DaoTestConfig;
+import com.foxminded.foxuniversity.dao.exceptions.EntityNotFoundException;
+import com.foxminded.foxuniversity.dao.exceptions.ExceptionsMessageConstants;
+import com.foxminded.foxuniversity.dao.exceptions.QueryRestrictedException;
 import com.foxminded.foxuniversity.domain.Course;
 import com.foxminded.foxuniversity.domain.Group;
 import org.apache.ibatis.jdbc.ScriptRunner;
@@ -9,6 +12,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -19,9 +23,10 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static com.foxminded.foxuniversity.dao.exceptions.ExceptionsMessageConstants.DELETION_RESTRICTED;
+import static com.foxminded.foxuniversity.dao.exceptions.ExceptionsMessageConstants.ENTITY_NOT_FOUND;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {DaoTestConfig.class})
@@ -39,7 +44,7 @@ class CourseDaoPostgresTest {
     private static List<Group> groups = new ArrayList<>();
 
     @BeforeAll
-    public static void initDataBase() throws Exception {
+    public static void initDataBase() {
         for (int i = 0; i < 3; i++) {
             courses.add(new Course(i + 1, "C-0" + (i + 1), "C-0" + (i + 1) + " course"));
             groups.add(new Group(i + 1, "gr-0" + (i + 1)));
@@ -75,6 +80,15 @@ class CourseDaoPostgresTest {
     }
 
     @Test
+    public void shouldThrowEntityNotFoundExceptionWhenCantFindEntity() {
+        int id = 10;
+        Throwable thrown = assertThrows(EntityNotFoundException.class, () -> courseDAO.getById(id));
+        String actualMessage = thrown.getMessage();
+        String expectedMessage = format(ENTITY_NOT_FOUND, "Course", id);
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
     public void shouldDeleteCourse() {
         List<Course> actual = courseDAO.getAll();
         assertEquals(courses, actual);
@@ -84,6 +98,14 @@ class CourseDaoPostgresTest {
         actual = courseDAO.getAll();
         List<Course> expected = courses.subList(0, 2);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowQueryRestrictedExceptionWhenCantDeleteEntity() {
+        Throwable thrown = assertThrows(QueryRestrictedException.class, () -> courseDAO.delete(courses.get(0)));
+        String actualMessage = thrown.getMessage();
+        String expectedMessage = format(DELETION_RESTRICTED, courses.get(0), "teachers");
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
