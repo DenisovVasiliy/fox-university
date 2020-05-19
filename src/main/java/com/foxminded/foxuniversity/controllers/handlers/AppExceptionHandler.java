f7bf171d99961e5d575b65525bbfa32c7136b9c9
VasyaDen <vasyaden@mail.ru>
2020-05-19 12:46:13 +0300

    AppExceptionHandler was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/main/java/com/foxminded/foxuniversity/controllers/handlers/AppExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.foxminded.foxuniversity.controllers.handlers;
+
+import com.foxminded.foxuniversity.dao.exceptions.EntityNotFoundException;
+import com.foxminded.foxuniversity.dao.exceptions.QueryNotExecuteException;
+import com.foxminded.foxuniversity.dao.exceptions.QueryRestrictedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    private static final String ERROR_PAGE = "error-page";
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public ModelAndView illegalArgument(RuntimeException ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("warnMessage", "Page not found. Please, check the link.");
+        modelAndView.setViewName(ERROR_PAGE);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    public ModelAndView notFound(EntityNotFoundException ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("warnMessage", ex.getMessage());
+        modelAndView.setViewName(ERROR_PAGE);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = {QueryNotExecuteException.class})
+    public ModelAndView notFound(QueryNotExecuteException ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("errorMessage", "Sorry, something went wrong...");
+        modelAndView.setViewName(ERROR_PAGE);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = {QueryRestrictedException.class})
+    public ModelAndView restricted(QueryRestrictedException ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("errorMessage", ex.getMessage());
+        modelAndView.setViewName(ERROR_PAGE);
+        return modelAndView;
+    }
+}
