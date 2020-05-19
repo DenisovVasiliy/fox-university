6224678283efa9ee39e96e785d98fccd12cb9f42
VasyaDen <vasyaden@mail.ru>
2020-05-19 18:18:45 +0300

    LessonController was updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/controllers/LessonController.java
+++ src/main/java/com/foxminded/foxuniversity/controllers/LessonController.java
@@ -1,11 +1,11 @@
 package com.foxminded.foxuniversity.controllers;
 
+import com.foxminded.foxuniversity.domain.Course;
 import com.foxminded.foxuniversity.domain.Lesson;
+import com.foxminded.foxuniversity.domain.Teacher;
+import com.foxminded.foxuniversity.service.CourseService;
 import com.foxminded.foxuniversity.service.LessonService;
-import com.foxminded.foxuniversity.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,29 +13,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
 
 @Controller
-@RequestMapping("/timetable")
-@PropertySource("classpath:lessonsTime.properties")
+@RequestMapping("/lessons")
 public class LessonController {
+    private CourseService courseService;
     private LessonService lessonService;
-    private TeacherService teacherService;
-
-    @Value("#{'${time}'.split(',')}")
-    private List<String> times;
 
     @Autowired
-    public LessonController(LessonService lessonService, TeacherService teacherService) {
+    public LessonController(CourseService courseService, LessonService lessonService) {
+        this.courseService = courseService;
         this.lessonService = lessonService;
-        this.teacherService = teacherService;
     }
-// It's full ***
-    @GetMapping
-    public String getTeacherTimetable(Model model) {
-        List<Lesson> lessons = lessonService.getByTeacher(teacherService.getById(parseInt("1")));
-        model.addAttribute("times", times);
-        return "lessons/timetable";
 
+    @GetMapping
+    public String showAllLessons(Model model) {
+        List<Lesson> lessons = lessonService.getAll();
+        model.addAttribute("title", "All lessons");
+        model.addAttribute("lessons", lessons);
+        return "lessons/lessons";
+    }
+
+    @GetMapping("/by-course")
+    public String showLessonsByCourse(Model model, int courseId) {
+        Course course = courseService.getById(courseId);
+        String title = format("Lessons of course %s", course.getName());
+        model.addAttribute("title", title);
+        model.addAttribute("lessons", course.getLessons());
+        return "lessons/lessons";
+    }
+
+    @GetMapping("/info")
+    public String showLessonById(Model model, int id) {
+        Lesson lesson = lessonService.getById(id);
+        model.addAttribute("lesson", lesson);
+        return "lessons/lesson-info";
     }
 }
