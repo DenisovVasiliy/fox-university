08b7ac05c95f47df1ddd1fa7c9f3b2dd093e2008
VasyaDen <vasyaden@mail.ru>
2020-05-20 18:13:30 +0300

    StudentController was updated (added Create&Update). Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/controllers/StudentController.java
+++ src/main/java/com/foxminded/foxuniversity/controllers/StudentController.java
@@ -1,29 +1,36 @@
 package com.foxminded.foxuniversity.controllers;
 
+import com.foxminded.foxuniversity.domain.Group;
 import com.foxminded.foxuniversity.domain.Student;
+import com.foxminded.foxuniversity.service.GroupService;
 import com.foxminded.foxuniversity.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+
+import static java.lang.String.format;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
     private StudentService studentService;
+    private GroupService groupService;
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, GroupService groupService) {
         this.studentService = studentService;
+        this.groupService = groupService;
     }
 
     @GetMapping
     public String showAllStudents(Model model) {
         List<Student> students = studentService.getAll();
         model.addAttribute("students", students);
+        model.addAttribute("newStudent", new Student());
         return "students/students";
     }
 
@@ -31,6 +38,21 @@ public class StudentController {
     public String showCourseById(Model model, int id) {
         Student student = studentService.getById(id);
         model.addAttribute("student", student);
+        model.addAttribute("editStudent", student);
         return "students/student-info";
+    }
+
+    @PostMapping("/new")
+    public ModelAndView saveStudent(@ModelAttribute("newStudent") Student student) {
+        studentService.save(student);
+        String redirect = format("redirect:/students/info/?id=%s", student.getId());
+        return new ModelAndView(redirect);
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView updateStudent(@ModelAttribute("editStudent") Student student, int id) {
+        studentService.update(student);
+        String redirect = format("redirect:/students/info/?id=%s", id);
+        return new ModelAndView(redirect);
     }
 }
