d870202acc2375e2fa1496e98f7dc85de75659b4
VasyaDen <vasyaden@mail.ru>
2020-05-22 12:48:51 +0300

    StudentController was updated. All CRUD-operations were added to StudentController. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/controllers/StudentController.java
+++ src/main/java/com/foxminded/foxuniversity/controllers/StudentController.java
@@ -20,6 +20,8 @@ public class StudentController {
     private StudentService studentService;
     private GroupService groupService;
 
+    private static final String REDIRECT_TO_INFO_PAGE = "redirect:/students/info/?id=%s";
+
     @Autowired
     public StudentController(StudentService studentService, GroupService groupService) {
         this.studentService = studentService;
@@ -35,24 +37,60 @@ public class StudentController {
     }
 
     @GetMapping("/info")
-    public String showCourseById(Model model, int id) {
+    public String showStudentById(Model model, int id) {
         Student student = studentService.getById(id);
         model.addAttribute("student", student);
         model.addAttribute("editStudent", student);
         return "students/student-info";
     }
 
+    @GetMapping("/assign")
+    public String showAssignStudentPage(Model model, int id) {
+        Student student = studentService.getById(id);
+        List<Group> groups = groupService.getAll();
+        model.addAttribute("student", student);
+        model.addAttribute("groups", groups);
+        model.addAttribute("newGroup", new Group(0));
+        return "students/student-assign";
+    }
+
+    @PostMapping("/assign")
+    public ModelAndView assignStudent(@ModelAttribute("newGroup") Group group, int studentId) {
+        Student student = studentService.getById(studentId);
+        if (student.getGroup() != null) {
+            student.setGroup(group);
+            studentService.updateAssignment(student);
+        } else {
+            studentService.assignToGroup(student, group);
+        }
+        String redirect = format(REDIRECT_TO_INFO_PAGE, student.getId());
+        return new ModelAndView(redirect);
+    }
+
+    @PostMapping("/delete")
+    public ModelAndView deleteStudent(@ModelAttribute("editStudent") Student student) {
+        studentService.delete(student);
+        return new ModelAndView("redirect:/students/");
+    }
+
     @PostMapping("/new")
     public ModelAndView saveStudent(@ModelAttribute("newStudent") Student student) {
         studentService.save(student);
-        String redirect = format("redirect:/students/info/?id=%s", student.getId());
+        String redirect = format(REDIRECT_TO_INFO_PAGE, student.getId());
         return new ModelAndView(redirect);
     }
 
     @PostMapping("/edit")
     public ModelAndView updateStudent(@ModelAttribute("editStudent") Student student, int id) {
         studentService.update(student);
-        String redirect = format("redirect:/students/info/?id=%s", id);
+        String redirect = format(REDIRECT_TO_INFO_PAGE, id);
+        return new ModelAndView(redirect);
+    }
+
+    @PostMapping("/delete-from-group/")
+    public ModelAndView deleteStudentFromGroup(@ModelAttribute("editStudent") Student student) {
+        studentService.deleteAssignment(student);
+        String redirect = format(REDIRECT_TO_INFO_PAGE, student.getId());
         return new ModelAndView(redirect);
     }
 }
