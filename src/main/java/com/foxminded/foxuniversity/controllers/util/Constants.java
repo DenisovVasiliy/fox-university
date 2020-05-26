ba157542f25575cd5209d84beab51d4ff6d2db94
VasyaDen <vasyaden@mail.ru>
2020-05-26 11:22:42 +0300

    Constants were updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/controllers/util/Constants.java
+++ src/main/java/com/foxminded/foxuniversity/controllers/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String SUCCESSFULLY_DELETED = "%s successfully deleted.";
     public static final String SUCCESS = "successMessage";
     public static final String DANGER = "dangerMessage";
+    public static final String HINT = "?hint=%s";
 }
