e408d0afe511d5b00f932991f2bcb843dcd1953c
VasyaDen <vasyaden@mail.ru>
2020-05-28 22:42:11 +0300

    TeacherController was updated. updateWithCourse() method was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/controllers/TeacherController.java
+++ src/main/java/com/foxminded/foxuniversity/controllers/TeacherController.java
@@ -1,8 +1,10 @@
 package com.foxminded.foxuniversity.controllers;
 
 import com.foxminded.foxuniversity.domain.Course;
+import com.foxminded.foxuniversity.domain.Lesson;
 import com.foxminded.foxuniversity.domain.Teacher;
 import com.foxminded.foxuniversity.service.CourseService;
+import com.foxminded.foxuniversity.service.LessonService;
 import com.foxminded.foxuniversity.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,23 +12,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.foxminded.foxuniversity.controllers.util.Constants.*;
 import static java.lang.String.format;
+import static java.util.Collections.singletonList;
 
 @Controller
 @RequestMapping("/teachers")
 public class TeacherController {
     private TeacherService teacherService;
     private CourseService courseService;
+    private LessonService lessonService;
     private static final String REDIRECT_TO_INFO_PAGE = "redirect:/teachers/info/?id=%s&hint=%s";
     private static final String DELETION_CANCELLED = "Deletion was cancelled: the teacher is assigned to some classes.";
 
     @Autowired
-    public TeacherController(TeacherService teacherService, CourseService courseService) {
+    public TeacherController(TeacherService teacherService,
+                             CourseService courseService,
+                             LessonService lessonService) {
         this.teacherService = teacherService;
         this.courseService = courseService;
+        this.lessonService = lessonService;
     }
 
     @GetMapping
@@ -65,9 +73,14 @@ public class TeacherController {
     @GetMapping("/edit")
     public String showEditTeacherPage(Model model, int id) {
         Teacher teacher = teacherService.getById(id);
-        List<Course> courses = courseService.getAll();
+        if (lessonService.getByTeacher(teacher).isEmpty()) {
+            List<Course> courses = courseService.getAll();
+            model.addAttribute("courses", courses);
+        } else {
+            model.addAttribute("courses", singletonList(teacher.getCourse()));
+            model.addAttribute(WARNING, TEACHERS_COURSE_CHANGE_WARNING);
+        }
         model.addAttribute("teacher", teacher);
-        model.addAttribute("courses", courses);
         return "teachers/teacher-edit";
     }
 
@@ -81,6 +94,13 @@ public class TeacherController {
     @PostMapping("/edit")
     public ModelAndView updateTeacher(@ModelAttribute("teacher") Teacher teacher) {
         teacherService.update(teacher);
+        String redirect = format(REDIRECT_TO_INFO_PAGE, teacher.getId(), SUCCESS);
+        return new ModelAndView(redirect);
+    }
+
+    @PostMapping("/editWithCourse")
+    public ModelAndView updateTeacherWithCourse(@ModelAttribute("teacher") Teacher teacher) {
+        teacherService.updateWithCourse(teacher);
         String redirect = format(REDIRECT_TO_INFO_PAGE, teacher.getId(), SUCCESS);
         return new ModelAndView(redirect);
     }
