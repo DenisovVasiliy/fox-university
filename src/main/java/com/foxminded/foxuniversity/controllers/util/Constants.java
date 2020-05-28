ee09577baf3ecd24d4d7e5a341e8f51a39c358a4
VasyaDen <vasyaden@mail.ru>
2020-05-28 22:38:26 +0300

    View cinstants were updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/controllers/util/Constants.java
+++ src/main/java/com/foxminded/foxuniversity/controllers/util/Constants.java
@@ -5,5 +5,7 @@ public class Constants {
     public static final String SUCCESSFULLY_DELETED = "%s successfully deleted.";
     public static final String SUCCESS = "successMessage";
     public static final String DANGER = "dangerMessage";
+    public static final String WARNING = "warningMessage";
     public static final String HINT = "?hint=%s";
+    public static final String TEACHERS_COURSE_CHANGE_WARNING = "Course change is not available because teacher has classes.";
 }
