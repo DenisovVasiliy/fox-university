920374d178631d054f93f4ee6358b9bc331e5e46
VasyaDen <vasyaden@mail.ru>
2020-06-04 15:52:22 +0300

    Course was updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/foxuniversity/domain/Course.java
+++ src/main/java/com/foxminded/foxuniversity/domain/Course.java
@@ -2,21 +2,29 @@ package com.foxminded.foxuniversity.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Data
 @RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "courses")
 public class Course {
     @NonNull
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @NonNull
+    @Column
     private String name;
     @NonNull
+    @Column
     private String description;
     @ToString.Exclude
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
     private List<Lesson> lessons = new ArrayList<>();
     @ToString.Exclude
     private List<Group> groups = new ArrayList<>();
